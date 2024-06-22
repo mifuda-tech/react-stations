9f80e2c25189d22cde23afdd1b2fa6e1b2bfa80c
mifuda-tech <mifuda.tech@gmail.com>
2024-06-22 13:07:38 +0900

    2-9:こんなかんじ？

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -3,30 +3,21 @@ import React, { useState, useEffect } from 'react'
 import DogImage from './Description'
 
 export const DogListContainer = () => {
-  const [breeds, setBreeds] = useState(
-    'https://images.dog.ceo/breeds/terrier-tibetan/n02097474_7572.jpg',
-  )
-
-  async function fetchData() {
-    const response = await fetch('https://dog.ceo/api/breeds/list/all')
-    const data = await response.json()
-    return data.message
-  }
-
-  const handleFetch = async () => {
-    const newDogList = await fetchData()
-    return newDogList
-  }
+  const [breeds, setBreeds] = useState()
 
   useEffect(() => {
-    setBreeds(handleFetch()), [breeds]
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(response => response.json())
+      .then(data => {
+        setBreeds(data.message)
+      }, [])
   })
 
   return (
     <div>
       <DogImage />
       <img src={breeds}></img>
-      <button onClick={useEffect}>更新</button>
+      <button>更新</button>
     </div>
   )
 }
